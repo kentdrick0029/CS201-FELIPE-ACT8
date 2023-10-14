9834cb4cb1d5e80535e092b31146504f0c487664
kentdrick0029 <kentdrickf@gmail.com>
2023-10-14 17:15:44 +0800

    feat: added javascript code into my html

---
+++ script.js
@@ -0,0 +1,12 @@
+const favSingers = ["David Kushner", "Boywithuke", "TheWeeknd"];
+const favNumbers = [29, 23, 12, 7];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers);
+console.log(favNumbers);
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
